79beb92c2e01cda0564b52661b4aca97e3cba919
lendle1028 <lendle.tseng.archive@gmail.com>
2022-05-05 12:17:14 +0800

    implementing

--- src/main/java/rocks/imsofa/rocksfx/button/SwitchButton.java
+++ src/main/java/rocks/imsofa/rocksfx/button/SwitchButton.java
@@ -5,10 +5,10 @@
 package rocks.imsofa.rocksfx.button;
 
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.EventHandler;
-import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -19,57 +19,82 @@ import javafx.scene.text.Text;
  *
  * @author lendle
  */
-public class SwitchButton extends Pane{
-    private boolean toggleChecked = true;
-    public SwitchButton(){
+public final class SwitchButton extends Pane {
+
+    private Circle leftCircle = null, rightCircle = null;
+    private Rectangle rectangle = null;
+    private Color mainColor = null, subColor = null;
+    private BooleanProperty checkedProperty = new SimpleBooleanProperty(false);
+    private Text textUI = null;
+
+    public SwitchButton(boolean initialState) {
         this.setPrefSize(80, 30);
-        this.setStyle("-fx-border-color: blue");
-        double width=80;
-        double height=30;
-        Pane toggleButton=this;
-        Rectangle rectangle = new Rectangle(height/2, 0, width-height, height);
+        checkedProperty.set(initialState);
+        double width = 80;
+        double height = 30;
+        mainColor = Color.web("#4dffb8");
+        subColor = Color.WHITE;
+        Pane toggleButton = this;
+        rectangle = new Rectangle(height / 2, 0, width - height, height);
         rectangle.xProperty().bind(Bindings.divide(toggleButton.heightProperty(), 2));
         rectangle.widthProperty().bind(Bindings.subtract(toggleButton.widthProperty(), toggleButton.heightProperty()));
         rectangle.heightProperty().bind(toggleButton.heightProperty());
-        rectangle.setFill(Color.GREEN);
-        Circle circle1 = new Circle(height/2, height/2, height/2);
-        circle1.centerXProperty().bind(Bindings.divide(toggleButton.heightProperty(), 2));
-        circle1.centerYProperty().bind(Bindings.divide(toggleButton.heightProperty(), 2));
-        circle1.radiusProperty().bind(Bindings.divide(toggleButton.heightProperty(), 2));
-        Circle circle2 = new Circle(width-height/2, height/2, height/2);
-        circle2.centerXProperty().bind(Bindings.subtract(toggleButton.widthProperty(),Bindings.divide(toggleButton.heightProperty(), 2)));
-        circle2.centerYProperty().bind(Bindings.divide(toggleButton.heightProperty(), 2));
-        circle2.radiusProperty().bind(Bindings.divide(toggleButton.heightProperty(), 2));
-        circle1.setFill(Color.GREEN);
-        circle1.setStroke(Color.GREEN);
-        circle2.setFill(Color.WHITE);
-        circle2.setStroke(Color.GREEN);
-        Text text = new Text(height/2, height/2+4, "ON");
-        text.setFill(Color.WHITE);
-        toggleButton.getChildren().addAll(rectangle, circle1, circle2, text);
-        toggleButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                toggleChecked = !toggleChecked;
-                if (toggleChecked) {
-                    circle1.setFill(Color.GREEN);
-                    circle1.setStroke(Color.GREEN);
-                    circle2.setFill(Color.WHITE);
-                    circle2.setStroke(Color.GREEN);
-                    text.setText("ON");
-                    text.setX(height/2);
-                    text.setY(height/2+4);
-                }else{
-                    circle2.setFill(Color.GREEN);
-                    circle2.setStroke(Color.GREEN);
-                    circle1.setFill(Color.WHITE);
-                    circle1.setStroke(Color.GREEN);
-                    text.setText("OFF");
-                    text.setX(height);
-                    text.setY(height/2+4);
-                }
+        rectangle.setFill(mainColor);
+        leftCircle = new Circle(height / 2, height / 2, height / 2);
+        leftCircle.centerXProperty().bind(Bindings.divide(toggleButton.heightProperty(), 2));
+        leftCircle.centerYProperty().bind(Bindings.divide(toggleButton.heightProperty(), 2));
+        leftCircle.radiusProperty().bind(Bindings.divide(toggleButton.heightProperty(), 2));
+        rightCircle = new Circle(width - height / 2, height / 2, height / 2);
+        rightCircle.centerXProperty().bind(Bindings.subtract(toggleButton.widthProperty(), Bindings.divide(toggleButton.heightProperty(), 2)));
+        rightCircle.centerYProperty().bind(Bindings.divide(toggleButton.heightProperty(), 2));
+        rightCircle.radiusProperty().bind(Bindings.divide(toggleButton.heightProperty(), 2));
+        leftCircle.setFill(mainColor);
+        leftCircle.setStroke(mainColor);
+        rightCircle.setFill(subColor);
+        rightCircle.setStroke(mainColor);
+        textUI = new Text(height / 2, height / 2 + 4, "ON");
+        textUI.setFill(subColor);
+        toggleButton.getChildren().addAll(rectangle, leftCircle, rightCircle, textUI);
+        toggleButton.setOnMouseClicked((MouseEvent event) -> {
+            checkedProperty.set(!checkedProperty.get());
+            if (checkedProperty.get()) {
+                showCheckedUI();
+            } else {
+                showUncheckedUI();
             }
-
         });
+        if (checkedProperty.get()) {
+            showCheckedUI();
+        } else {
+            showUncheckedUI();
+        }
+    }
+
+    protected void showUncheckedUI() {
+        leftCircle.setFill(mainColor);
+        leftCircle.setStroke(mainColor);
+        rightCircle.setFill(subColor);
+        rightCircle.setStroke(mainColor);
+        textUI.setText("ON");
+        textUI.setX(this.getHeight() / 2);
+        textUI.setY(this.getHeight() / 2 + 4);
+    }
+
+    protected final void showCheckedUI() {
+        rightCircle.setFill(mainColor);
+        rightCircle.setStroke(mainColor);
+        leftCircle.setFill(subColor);
+        leftCircle.setStroke(mainColor);
+        textUI.setText("OFF");
+        textUI.setX(this.getHeight());
+        textUI.setY(this.getHeight() / 2 + 4);
+    }
+
+    public boolean isChecked() {
+        return checkedProperty.get();
+    }
+
+    public BooleanProperty checkedProperty() {
+        return checkedProperty;
     }
 }
